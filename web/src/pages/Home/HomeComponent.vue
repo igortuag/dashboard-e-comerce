33f7da6245c9e23b59e92b2a10606e2f30d473b6
Igor Tuag <igortuag@gmail.com>
2020-07-26 16:35:27 -0300

    Add rotas login e home

---
+++ web/src/pages/Home/HomeComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Home da Dashboard</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ web/src/pages/Login/LoginComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Login do nosso Sistema</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
